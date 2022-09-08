18cceadd46588bb084f7d7a5eacd713d4761fc64
Ogulcan Keskin <ogulcankeskin93@gmail.com>
2022-09-08 10:46:21 +0200

    test

--- Sources/ColorPackage/ColorPackage.swift
+++ Sources/ColorPackage/ColorPackage.swift
@@ -1,5 +1,7 @@
 public struct ColorPackage {
     public private(set) var text = "Hello, World!"
+    public private(set) var hello = "Hello, World!"
+    public private(set) var new = "Hello, World!"
 
     public init() {
     }
